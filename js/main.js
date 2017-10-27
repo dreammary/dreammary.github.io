8d9249df2544ac8294c59f69650f6b2ac67bc187
asaychuk <saychuk.andriy@gmail.com>
2017-10-27 16:04:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,137 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+    $("#testimonial-slider-1").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[980,1],
+        itemsTablet:[768,1],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[980,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
+
+    var cntForm = $("form#cntForm");
+    cntForm.submit(function(event){
+      event.preventDefault();
+
+      // Change to your service ID, or keep using the default service
+      var service_id = "gmail";
+      var template_id = "dreemmary";
+
+      cntForm.find(".mainBtn").val("Sending ...");
+      emailjs.sendForm(service_id,template_id,"cntForm")
+        .then(function(){
+          cntForm.find(".mainBtn").val("Send Message");
+        }, function(err) {
+          alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+          cntForm.find(".mainBtn").val("Send Message");
+        });
+      return false;
+    });
+	/************** Toggle *********************/
+    // Cache selectors
+    var lastId,
+        topMenu = $(".menu-first"),
+        topMenuHeight = 50,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+          
+          if($(this).hasClass('external')) {
+            return;
+          }
+            
+          var item = $($(this).attr("href"));
+          if (item.length) { return item; }
+        });
+
+    // Bind click handler to menu items
+    // so we can get a fancy scroll animation
+    menuItems.click(function(e){
+      var href = $(this).attr("href"),
+          offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+      $('html, body').stop().animate({ 
+          scrollTop: offsetTop
+      }, 300);
+      e.preventDefault();
+    });
+
+    // Bind to scroll
+    $(window).scroll(function(){
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+topMenuHeight;
+       
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop){
+           return this;
+         }
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+       
+       if (lastId !== id) {
+           lastId = id;
+           // Set/remove active class
+           menuItems
+             .parent().removeClass("active")
+             .end().filter("[href=#"+id+"]").parent().addClass("active");
+       }                   
+    });
+
+
+
+    $(window).scroll(function(){
+         $('.main-header').toggleClass('scrolled', $(this).scrollTop() > 1);
+     });
+
+
+
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop: 0}, 'slow');
+        return false;
+    });
+
+
+    $('.flexslider').flexslider({
+      slideshow: true,
+      slideshowSpeed: 3000,  
+      animation: "fade",
+      directionNav: false,
+    });
+
+
+    $('.toggle-menu').click(function(){
+        $('.menu-first').toggleClass('show');
+        // $('.menu-first').slideToggle();
+    });
+
+    $('.menu-first li a').click(function(){
+      $('.menu-first').removeClass('show');
+    });
+
+
+    /************** LightBox *********************/
+      $(function(){
+        $('[data-rel="lightbox"]').lightbox();
+      });
+
+
+});
